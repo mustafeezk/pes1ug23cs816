615ccf2597521db1b9e8ec27abfa2f300a38a788
Mustafeez <120350773+mustafeezk@users.noreply.github.com>
2025-03-15 12:26:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Sahil;\n";
+  cout << "Hello, MOHAMMED MUSTAFEEZ KITTUR;\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
